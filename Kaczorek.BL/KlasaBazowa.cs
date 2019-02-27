c8e6b3645bdb2b0f5e49dfc68f7d7ff3b65a63b7
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-02-27 14:44:02 +0100

    klasa bazowa dla bl

---
+++ Kaczorek.BL/KlasaBazowa.cs
@@ -0,0 +1,24 @@
+﻿namespace Kaczorek.BL
+{
+    public abstract class KlasaBazowa
+    {
+        public bool JestNowy { get; set; }
+        public bool MaZmiany { get; set; }
+        public StanObiektuOpcje StanObiektu { get; set; }
+        public bool DanePrawidlowe
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+
+    public enum StanObiektuOpcje
+    {
+        Aktywny,
+        Usunięty
+    }
+
+}
+
